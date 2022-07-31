699e014808d8f24820e4c0b1446a1ef3ffa7c1f4
Calvin Liu <calvin.liu68@gmail.com>
2022-07-31 14:19:27 -0600

    add chart.js

--- website/brainauth/src/App.tsx
+++ website/brainauth/src/App.tsx
@@ -1,7 +1,45 @@
+import { Line } from "react-chartjs-2";
+import { CategoryScale, Chart, registerables } from "chart.js";
 import "./App.css";
 import { Forms } from "./Form";
 
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Brain data",
+    },
+  },
+};
+
+const labels = [
+  { timestamp: 1, value: 1 },
+  { timestamp: 1.09, value: 3 },
+  { timestamp: 1.13, value: 7 },
+  { timestamp: 1.16, value: 4 },
+  { timestamp: 1.21, value: -2 },
+];
+
+export const data = {
+  labels: labels.map((value) => value.timestamp),
+  datasets: [
+    {
+      label: "EEG Data",
+      data: labels.map((value) => value.value),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
+
 function App() {
+  Chart.register(CategoryScale);
+  Chart.register(...registerables);
+
   return (
     <div className="App">
       <Forms
@@ -9,6 +47,7 @@ function App() {
           console.log(firstName, lastName);
         }}
       />
+      <Line options={options} data={data} />
     </div>
   );
 }
