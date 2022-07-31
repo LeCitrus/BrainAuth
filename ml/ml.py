826278eb7f185468c6523ad1288a9e285b559577
LeCitrus <curtis.tl.kan@gmail.com>
2022-07-30 19:25:12 -0600

    Co-authored-by: Aneeljyot Alagh <ajsingh1012@users.noreply.github.com>

---
+++ ml/ml.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
