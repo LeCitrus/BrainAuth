83a0c875c24017deac8d007d531aad5877770b5c
Calvin Liu <calvin.liu68@gmail.com>
2022-07-31 09:18:31 -0600

    upgrade node

--- website/brainauth/src/App.tsx
+++ website/brainauth/src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Forms} from "./Form"
+import "./App.css";
+import { Forms } from "./Form";
 
 function App() {
   return (
     <div className="App">
-      <Forms onSubmit={({firstName, lastName}) =>{
-        console.log(firstName, lastName)
-
-      }}/>
+      <Forms
+        onSubmit={({ firstName, lastName }) => {
+          console.log(firstName, lastName);
+        }}
+      />
     </div>
   );
 }
