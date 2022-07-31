03baafe8be185ffced059f942409752002ee2430
Calvin Liu <calvin.liu68@gmail.com>
2022-07-31 17:10:19 -0600

    add graph data processing

--- website/brainauth/src/components/Form.tsx
+++ website/brainauth/src/components/Form.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 import { Formik, Form } from "formik";
+import { DataPoint } from "../models/types";
 
 interface Values {
   firstName: string;
@@ -9,74 +10,91 @@ interface Values {
 }
 
 interface Props {
+  setData: React.Dispatch<React.SetStateAction<DataPoint[] | undefined>>;
   onSubmit: (values: Values) => void;
 }
 
+export const Forms: React.FC<Props> = ({ setData, onSubmit }) => {
+  const [file, setFile] = useState();
 
-export const Forms: React.FC<Props> = ({onSubmit}) => {
+  const fileReader = new FileReader();
 
-    const [file, setFile] = useState();
+  const handleOnChange = (e: any) => {
+    setFile(e.target.files[0]);
+  };
 
-    const fileReader = new FileReader();
+  const parseCsvOutput = (output: string) => {
+    return output.split("\n").map((entry) => {
+      let data = entry.split(",");
+      if (data) {
+        return {
+          timestamp: data[0] ? parseFloat(data[0].replace('"', "")) : 0,
+          value: data[1] ? parseFloat(data[1].replace('"', "")) : 0,
+        };
+      } else {
+        return {
+          timestamp: 0,
+          value: 0,
+        };
+      }
+    });
+  };
 
-    const handleOnChange = (e: any) => {
-        setFile(e.target.files[0]);
-    };
-    
-    const handleCsvSubmit = (e : any) => {
+  const handleCsvSubmit = (e: any) => {
+    if (file) {
+      fileReader.onload = function (event: any) {
+        const csvOutput = event.target.result;
+        console.log(csvOutput);
+        let parsed = parseCsvOutput(csvOutput);
+        setData(parsed);
+      };
 
-        if (file) {
-            fileReader.onload = function (event : any) {
-                const csvOutput = event.target.result;
-                console.log(csvOutput)
-            };
+      fileReader.readAsText(file);
+    }
+  };
 
-            fileReader.readAsText(file);
-        }
-    };
-
-    return (
-    <Formik 
-    initialValues={{firstName: '', lastName: ''}} 
-    onSubmit={values => {
+  return (
+    <Formik
+      initialValues={{ firstName: "", lastName: "" }}
+      onSubmit={(values) => {
         onSubmit(values);
         handleCsvSubmit(file);
-    }}
+      }}
     >
-     {({values, handleChange, handleBlur}) => (
-            <Form>
-                <div>
-                <TextField
-                 placeholder="First Name"
-                 name="firstName" 
-                 value={values.firstName} 
-                 onChange={handleChange}
-                 onBlur={handleBlur}
-                 />
-                </div>
-                <div>
-                 <TextField
-                 placeholder="Last Name"
-                 name="lastName" 
-                 value={values.lastName} 
-                 onChange={handleChange}
-                 onBlur={handleBlur}
-                 />
-                </div>
-                <div>
-                 <label id="fileSelect"></label>
-                    <input id="fileSelect" type="file" 
-                    accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, 
+      {({ values, handleChange, handleBlur }) => (
+        <Form>
+          <div>
+            <TextField
+              placeholder="First Name"
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div>
+            <TextField
+              placeholder="Last Name"
+              name="lastName"
+              value={values.lastName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div>
+            <label id="fileSelect"></label>
+            <input
+              id="fileSelect"
+              type="file"
+              accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, 
                     application/vnd.ms-excel, .txt"
-                    onChange={handleOnChange} />
-                    
-                </div>
+              onChange={handleOnChange}
+            />
+          </div>
 
-        <Button type="submit">submit</Button>
-
-            </Form>
-        )}
-        
+          <Button type="submit">submit</Button>
+        </Form>
+      )}
     </Formik>
   );
 };
