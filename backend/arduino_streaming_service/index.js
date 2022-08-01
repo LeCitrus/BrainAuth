aff5e0c644c1e10d1a9b9c7faf772bb6f63eb964
Calvin Liu <calvin.liu68@gmail.com>
2022-07-31 21:11:17 -0600

    try streaming data

--- backend/arduino_streaming_service/index.js
+++ backend/arduino_streaming_service/index.js
@@ -1,7 +1,15 @@
 import { SerialPort } from "serialport";
+import { SerialPortStream } from "@serialport/stream";
+import * as fs from "fs";
 
 let BAUD_RATE = 115200;
 let PATH = "COM4";
+
+async function logChunks(readable) {
+  for await (const chunk of readable) {
+    console.log(chunk);
+  }
+}
 
 // Create a port
 try {
@@ -9,10 +17,17 @@ try {
     path: PATH,
     baudRate: BAUD_RATE,
   });
+  // const stream = new SerialPortStream({
+  //   path: PATH,
+  //   baudRate: BAUD_RATE,
+  // });
+  // const r = stream.read();
+  // console.log(r);
   console.log("port successfully found: ", port);
   const l = port.read(10);
   console.log(typeof l);
   console.log(l);
+  logChunks(l);
 } catch (err) {
   console.log("Error connecting to port: ", err);
 }
